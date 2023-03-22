4fc758932610f544cc8dcc2543f9266171286feb
Patrick <patrickedifor10@gmail.com>
2023-03-22 03:41:21 +0000

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * int - adds two numbers
+ * @a: parameter 1
+ * @b: parameter 2
+ * Return: sum a + b
+ */
+int add(int a, int b);
+{
+	return (a + b);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void times_table(void);
+{
+int x, y, z, u, d;
+for (x = z;
