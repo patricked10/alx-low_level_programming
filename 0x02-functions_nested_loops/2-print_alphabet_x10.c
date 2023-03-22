56cfa2d96358b7ef02ea809d8eb867124bb23bee
Patrick <patrickedifor10@gmail.com>
2023-03-22 00:19:21 +0000

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,14 +6,12 @@
 void print_alphabet_x10(void)
 {
 	int i;
-	int j;
+	char j;
 
-	for (j = 1; i <= 10; i++)
+	for (i = 1 ; i <= 10 ; i++)
 	{
-		for (j + 97; j <= 122; j++)
-		{
+		for (j + 'a' ; j <= 'z' ; j++)
 			_putchar(j);
-		}
 		_putchar('\n');
 	}
 }
